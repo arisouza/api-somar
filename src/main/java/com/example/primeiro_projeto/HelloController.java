4ce1cee80bd0c532f8aef0641b2a80d0e5517c1f
Ariane Souza <ari.souza02@outlook.com>
2025-03-14 11:37:14 -0300

    Endpoint somar

--- src/main/java/com/example/primeiro_projeto/HelloController.java
+++ src/main/java/com/example/primeiro_projeto/HelloController.java
@@ -21,4 +21,12 @@ public class HelloController {
     public String bemVindo(@PathVariable String nome) {
         return "Bem vindo, " + nome + "!";
     }
+    @GetMapping("/somar/{num1}/{num2}")
+    public String somar(@PathVariable String num1, @PathVariable String num2) {
+        int numero1 = Integer.parseInt(num1);
+        int numero2 = Integer.parseInt(num2);
+        int resultado = numero1 + numero2;
+        return "O resultado da soma de " + numero1 + " + " + numero2 + " é " + resultado;
+    }
+    
 }
